30b627b086cc56c124c8a9619cd061806c7010d2
Blake McHale <mchale.blake@gmail.com>
2020-11-10 00:07:21 -0500

    Add working gazebo simulation environment with apriltag detection and drone flight simulation

--- README.md
+++ README.md
@@ -2,34 +2,27 @@
 EECE5552 Assistive Robotics Final Project
 
 ## Installation
-[AirSim](https://microsoft.github.io/AirSim/build_linux/)
 
 ``` bash
-sudo apt install git-lfs ros-noetic-apriltag ros-noetic-apriltag-ros
+sudo apt install ros-noetic-apriltag-ros
 ```
 
 ## Building
 
-**NOTE: Replace /path/to/AirSim with your path**
-
 ``` bash
-source /path/to/AirSim/ros/devel/setup.bash
-cd final_ws
 catkin build
 ```
 
 ## Running
 
-Start Unreal Engine and open Blocks.uproject in Environments/Blocks.
-
-Copy our settings.json to the Documents/AirSim folder
-
-``` bash
-cp drone.json ~/Documents/AirSim/settings.json
-```
-
 Run our nodes and start the drone. Remember to always call `source devel/setup.bash`
 
 ``` bash
+source devel/setup.bash
+roslaunch sjtu_drone simple.launch
+```
+
+``` bash
+source devel/setup.bash
 roslaunch alleye start_drone.launch
 ```
--- final_ws/src/alleye/src/start_drone.py
+++ final_ws/src/alleye/src/start_drone.py
@@ -1,50 +1,48 @@
 #!/usr/bin/env python3
-from airsim_ros_pkgs.srv import Takeoff, Land, SetLocalPosition
+# TODO: Get rid of airsim references
+# from airsim_ros_pkgs.srv import Takeoff, Land, SetLocalPosition
 from apriltag_ros.msg import AprilTagDetectionArray
+from std_msgs.msg import Empty, Bool
+from geometry_msgs.msg import Twist
 import rospy
 
 class Drone:
     def __init__(self):
-        self.namespace = '/airsim_node/drone_1'
-        rospy.wait_for_service(self.namespace + '/takeoff')
-        rospy.wait_for_service(self.namespace + '/land')
-
-        # Setup services
-        self.srv_takeoff = rospy.ServiceProxy(self.namespace + '/takeoff', Takeoff)
-        self.srv_land = rospy.ServiceProxy(self.namespace + '/land', Land)
-        self.srv_local_goal = rospy.ServiceProxy('/airsim_node/local_position_goal/override', SetLocalPosition)
+        self.namespace = 'drone'
         self.subTag = rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.tagCallback)
+        self.pub_takeoff = rospy.Publisher(f"/{self.namespace}/takeoff", Empty, queue_size=10)
+        self.pub_land = rospy.Publisher(f"/{self.namespace}/land", Empty, queue_size=10)
+        self.pub_posctrl = rospy.Publisher(f"/{self.namespace}/posctrl", Bool, queue_size=10)
+        self.pub_cmdvel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+
+        while not self.pub_takeoff.get_num_connections() or not self.pub_land.get_num_connections() \
+            or not self.pub_posctrl.get_num_connections or not self.pub_cmdvel.get_num_connections():
+            rospy.logwarn_throttle(1.0, "Waiting for publishers to connect")
 
     def start(self):
-        # Initialize and takeoff drone
-        self.takeoff()
-
+        # self.takeoff()
         # Move to point above area we want to monitor
-        self.set_local_position(0.0, 0.0, -10.0, 0.0)
+        self.set_local_position(0.0, 0.0, 5.0, 0.0)
 
     def takeoff(self):
-        try:
-            resp = self.srv_takeoff(False)
-            return resp.success 
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+        rospy.loginfo("Taking off")
+        self.pub_takeoff.publish()
+        rospy.loginfo("Took off")
 
     def land(self):
-        try:
-            resp = self.srv_land(False)
-            return resp.success 
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+        self.pub_land.publish()
 
     def set_local_position(self, x, y, z, yaw, vehicle_name=''):
         """
-        Sets local position in NED frame.
+        Sets local position in XYZ frame.
         """
-        try:
-            resp = self.srv_local_goal(x, y, z, yaw, vehicle_name)
-            return resp.success 
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+        self.pub_posctrl.publish(True)
+        twist = Twist()
+        twist.linear.x = x
+        twist.linear.y = y
+        twist.linear.z = z
+        self.pub_cmdvel.publish(twist)
+        rospy.loginfo(f"Sent waypoint x: {x} y: {y} z: {z} yaw: {yaw}")
 
     def tagCallback(self, data):
         pass
